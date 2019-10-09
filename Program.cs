7252d2f17f7cd10a5314102b4f34824e28d39ef0
Fredrik Andersson <fredrik.andersson.9008@gmail.com>
2019-10-09 10:53:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace VehicleInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
